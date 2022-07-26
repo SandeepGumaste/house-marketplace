a039ebcd2b203283dfda9de4e32ac898dc775176
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2022-07-26 09:30:07 +0530

    listing page

---
+++ src/pages/Listing.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { getDoc, doc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { db } from "../firebase.config";
+
+const Listing = () => {
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(null);
+
+  const navigate = useNavigate();
+  const params = useParams();
+  console.log(params);
+  const auth = getAuth();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        const docRef = doc(db, "listings", params.listingId);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          console.log(docSnap.data());
+          setListing(docSnap.data());
+          setLoading(false);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchListing();
+
+    return () => {};
+  }, [navigate, params.listingId]);
+
+  return <div>Listing</div>;
+};
+
+export default Listing;
